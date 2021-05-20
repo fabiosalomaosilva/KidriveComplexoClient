4272fe7cf01f61bdc9819ee270e7187da789e457
Fábio Salomão Silva <fabio@arquivarnet.com.br>
2021-05-19 22:04:14 -0500

    Adicionar arquivos de projeto.

---
+++ KidriveComplexoClient/Program.cs
@@ -0,0 +1,25 @@
+using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+
+namespace KidriveComplexoClient
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddBlazoredLocalStorage();
+            builder.Services.AddScoped<AuthenticationStateProvider, ApiAuthStateProvider>();
+            await builder.Build().RunAsync();
+        }
+    }
+}
