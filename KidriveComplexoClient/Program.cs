8d96832921db123250f8f236018da72d03ff0429
Fábio Salomão Silva <fabio@arquivarnet.com.br>
2021-05-20 23:01:24 -0500

    update

--- KidriveComplexoClient/Program.cs
+++ KidriveComplexoClient/Program.cs
@@ -16,8 +16,8 @@ namespace KidriveComplexoClient
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
-            builder.Services.AddMudServices();
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddMudServices();
             builder.Services.AddBlazoredLocalStorage();
             builder.Services.AddScoped<AuthenticationStateProvider, ApiAuthStateProvider>();
             await builder.Build().RunAsync();
