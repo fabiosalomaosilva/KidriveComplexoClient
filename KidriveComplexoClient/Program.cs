d9949f674e769f987c28562fdb1ce6c288ad7f17
Fábio Salomão Silva <fabio@arquivarnet.com.br>
2021-05-26 18:25:00 -0500

    update

--- KidriveComplexoClient/Program.cs
+++ KidriveComplexoClient/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using MudBlazor.Services;
+using Syncfusion.Blazor;
 using System;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -17,8 +18,11 @@ namespace KidriveComplexoClient
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("NDUwNzIzQDMxMzkyZTMxMmUzMFRJTnVhTkZFdGdodTJxL0loVFliTzRIOUN5TXRIM1dkTEs0QVYrN283Q009");
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://southamerica-east1-kidrivecomplexo.cloudfunctions.net/api/") });
             builder.Services.AddMudServices();
+            builder.Services.AddSyncfusionBlazor();
+
             builder.Services.AddAuthorizationCore();
             builder.Services.AddBlazoredLocalStorage();
             builder.Services.AddScoped<AuthenticationStateProvider, ApiAuthStateProvider>();
